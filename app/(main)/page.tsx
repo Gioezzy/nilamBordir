c506ce309e6f23b4cd8e6de438dc1473edf34812
gioezzy <giovanniyuda29@gmail.com>
2025-11-01 23:24:00 +0700

    feat(app): add main layout and home page

---
+++ app/(main)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="container mx-auto py-10 space-y-12">
+      <h1 className="text-4xl font-bold text-center">Welcome to Nilam Bordir!</h1>
+    </main>
+  );
+}
