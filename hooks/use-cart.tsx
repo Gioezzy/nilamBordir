7d4b9bc368512b6cd51590e8e019048e294ce679
gioezzy <giovanniyuda29@gmail.com>
2025-11-03 22:51:45 +0700

    chore: update use-cart hook

--- hooks/use-cart.tsx
+++ hooks/use-cart.tsx
@@ -1,19 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-'use client'
+'use client';
 
-import { useState } from "react"
-
-
-export function useCart() {
-  const [items] = useState([
-    { id: '1', name: 'Salempang Bordir', quantity: 2},
-    { id: '2', name: 'Bordir Logo', quantity: 1},
-  ])
-
-  return {
-    items,
-    addItem: (item: any) => {},
-    removeItem: (id: string) => {},
-    clearCart: () => {},
-  }
-}
+export { useCart } from '@/context/cart-context';
