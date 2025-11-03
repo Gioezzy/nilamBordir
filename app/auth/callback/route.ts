11bfc75a535d6bd889563266da6274fd41bf5435
gioezzy <giovanniyuda29@gmail.com>
2025-11-03 22:56:38 +0700

    feat: add auth callback route

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,42 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin} = new URL(request.url)
+  const code = searchParams.get('code')
+  const next = searchParams.get('next') ?? '/dashboard'
+
+  if(code){
+    const supabase = await createClient()
+    const { error, data} = await supabase.auth.exchangeCodeForSession(code)
+
+    if (!error && data.user){
+      const { data: profile} = await supabase
+      .from('profiles')
+      .select('id')
+      .eq('id', data.user.id)
+      .single()
+
+      if (!profile){
+        await supabase.from('profiles').insert({
+          id: data.user.id,
+          full_name: data.user.user_metadata.full_name || data.user.email?.split('@')[0],
+          role: 'customer',
+        })
+      }
+
+      const forwardedHost = request.headers.get('x-forwarded-host')
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+
+      if (isLocalEnv){
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost){
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
+    }
+  }
+
+  return NextResponse.redirect(`${origin}/login?error=authentication_failed`)
+}
